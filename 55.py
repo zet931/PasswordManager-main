28f7a4902d81d66a0d982efb721403c2a75b3e0d
zet <koptevmisha7@gmail.com>
2024-04-21 21:08:54 +0300

    база

--- 55.py
+++ 55.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask("zen")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  intro = db.Column(db.String(300), nullable=False)
+  text = db.Column(db.Text, nullable=False)
+  date = db.Column(db.DateTime, default=datetime)
+
+  def __repr__(self):
+    return "<Article %r>" % self.id
+
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template("index.html")
 
 if "zen" == 'main':
   app.run(debug=True)
